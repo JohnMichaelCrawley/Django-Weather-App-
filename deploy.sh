cda2cbeee16fa44e96ff326aa8909149f7f2cd36
Aflynn987 <aflynn987@gmail.com>
2023-04-07 09:08:11 +0100

    testing security

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,6 @@ source venv/bin/activate
 pip3 install django
 pip3 install django-sslserver
 
-echo $PRIVATE_KEY > privatekey.pem
-echo $CERT > cert.pem
+openssl req -x509 -newkey rsa:4096 -nodes -keyout privatekey.pem -out cert.pem -days 365 -subj "/C=IE/ST=Leinster/L=Dublin/O=National College of Ireland/OU=School of Computing/CN=ncirl.ie"
 # Start the application with the process name weather_app using pm2
 python manage.py runsslserver 0.0.0.0:8443 --certificate cert.pem --key privatekey.pem > /dev/null 2>&1 &
